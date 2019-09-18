753df3c724c69fcbe66089b65e06df0ffd2b5697
estevez-dev <vyalov.egor@gmail.com>
2019-09-18 21:18:24 +0300

    v.0.6.7

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ EventBus eventBus = new EventBus();
 final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
 const String appName = "HA Client";
-const appVersion = "0.6.6";
+const appVersion = "0.6.7";
 
 void main() async {
   FlutterError.onError = (errorDetails) {
