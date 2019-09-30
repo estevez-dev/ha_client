92165aa7ed4eb8dc418577612e4c170a2aeeb236
estevez-dev <vyalov.egor@gmail.com>
2019-09-30 20:30:52 +0300

    Remove intents

--- android/app/src/main/java/com/keyboardcrumbs/hassclient/MainActivity.java
+++ android/app/src/main/java/com/keyboardcrumbs/hassclient/MainActivity.java
@@ -6,30 +6,10 @@ import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugins.share.FlutterShareReceiverActivity;
 
 public class MainActivity extends FlutterShareReceiverActivity {
-  private static final String CHANNEL = "haclient.deeplink/channel";
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-    
-    Intent intent = getIntent();
-    Uri data = intent.getData();
-
-    new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
-            new MethodChannel.MethodCallHandler() {
-              @Override
-              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-                if (call.method.equals("initialLink")) {
-                  if (startString != null) {
-                    result.success(startString);
-                  }
-                }
-              }
-            });
-
-    if (data != null) {
-      startString = data.toString();
-    }
   }
 }
