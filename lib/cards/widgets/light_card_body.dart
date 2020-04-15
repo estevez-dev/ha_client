82d7aeba026451064e2138277f1063c707b23d26
Yegor Vialov <vyalov.egor@gmail.com>
2020-04-15 18:23:29 +0000

    Some cleanup

--- lib/cards/widgets/light_card_body.dart
+++ lib/cards/widgets/light_card_body.dart
@@ -24,13 +24,7 @@ class _LightCardBodyState extends State<LightCardBody> {
       widthFactor: 0.5,
       child: Container(
         //color: Colors.redAccent,
-        child: SingleCircularSlider(
-            255,
-            entity.brightness ?? 0,
-          baseColor: Colors.white,
-          handlerColor: Colors.blue[200],
-          selectionColor: Colors.blue[100],
-        ),
+        child: Text('Pffffff'),
       ),
     );
 
