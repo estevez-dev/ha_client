e7cce01ca9b1399e40e3d967914562138c814e2b
Yegor Vialov <vyalov.egor@gmail.com>
2020-05-05 12:34:20 +0000

    Remove beta

--- lib/main.dart
+++ lib/main.dart
@@ -159,7 +159,7 @@ final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
 const String appName = "HA Client";
 const appVersionNumber = "1.0.0";
-const appVersionAdd = "beta";
+const appVersionAdd = "";
 const appVersion = "$appVersionNumber$appVersionAdd";
 
 Future<void> _reportError(dynamic error, dynamic stackTrace) async {
