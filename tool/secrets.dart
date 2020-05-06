ded60a2867d19939d83eae2a6162e64d8ed25cc7
Yegor Vialov <vyalov.egor@gmail.com>
2020-05-06 18:22:17 +0000

    not null

--- tool/secrets.dart
+++ tool/secrets.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 Future<void> main() async {
   final config = {
     'syncfusion_license_key': Platform.environment['SYNCFUSION_LICENSE_KEY'],
-    'version_type': Platform.environment['HA_CLIENT_VERSION_TYPE']
+    'version_type': Platform.environment['HA_CLIENT_VERSION_TYPE'] ?? ''
   };
 
   final filename = 'lib/.secrets.dart';
