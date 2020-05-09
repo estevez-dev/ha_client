4fbf58e7070dd65c5491ac914b1b53681b661c4b
Yegor Vialov <vyalov.egor@gmail.com>
2020-05-09 11:39:53 +0000

    Change default hold action to more-info

--- lib/entities/entity_wrapper.class.dart
+++ lib/entities/entity_wrapper.class.dart
@@ -155,3 +155,63 @@ class EntityWrapper {
   }
 
 }
+
+class EntityUIAction {
+  static const moreInfo = 'more-info';
+  static const toggle = 'toggle';
+  static const callService = 'call-service';
+  static const navigate = 'navigate';
+  static const none = 'none';
+
+  String tapAction = EntityUIAction.moreInfo;
+  String tapNavigationPath;
+  String tapService;
+  Map<String, dynamic> tapServiceData;
+  String holdAction = EntityUIAction.moreInfo;
+  String holdNavigationPath;
+  String holdService;
+  Map<String, dynamic> holdServiceData;
+  String doubleTapAction = EntityUIAction.none;
+  String doubleTapNavigationPath;
+  String doubleTapService;
+  Map<String, dynamic> doubleTapServiceData;
+
+  EntityUIAction({rawEntityData}) {
+    if (rawEntityData != null) {
+      if (rawEntityData["tap_action"] != null) {
+        if (rawEntityData["tap_action"] is String) {
+          tapAction = rawEntityData["tap_action"];
+        } else {
+          tapAction =
+              rawEntityData["tap_action"]["action"] ?? EntityUIAction.moreInfo;
+          tapNavigationPath = rawEntityData["tap_action"]["navigation_path"];
+          tapService = rawEntityData["tap_action"]["service"];
+          tapServiceData = rawEntityData["tap_action"]["service_data"];
+        }
+      }
+      if (rawEntityData["hold_action"] != null) {
+        if (rawEntityData["hold_action"] is String) {
+          holdAction = rawEntityData["hold_action"];
+        } else {
+          holdAction =
+              rawEntityData["hold_action"]["action"] ?? EntityUIAction.none;
+          holdNavigationPath = rawEntityData["hold_action"]["navigation_path"];
+          holdService = rawEntityData["hold_action"]["service"];
+          holdServiceData = rawEntityData["hold_action"]["service_data"];
+        }
+      }
+      if (rawEntityData["double_tap_action"] != null) {
+        if (rawEntityData["double_tap_action"] is String) {
+          doubleTapAction = rawEntityData["double_tap_action"];
+        } else {
+          doubleTapAction =
+              rawEntityData["double_tap_action"]["action"] ?? EntityUIAction.none;
+          doubleTapNavigationPath = rawEntityData["double_tap_action"]["navigation_path"];
+          doubleTapService = rawEntityData["double_tap_action"]["service"];
+          doubleTapServiceData = rawEntityData["double_tap_action"]["service_data"];
+        }
+      }
+    }
+  }
+
+}
