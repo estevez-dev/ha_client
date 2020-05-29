096e714a04f9653e3dfff2ec61c9507dfc6053bb
Yegor Vialov <vyalov.egor@gmail.com>
2020-05-29 16:52:57 +0000

    Real fullscreen for camera view

--- lib/pages/fullscreen.page.dart
+++ lib/pages/fullscreen.page.dart
@@ -1,18 +1,37 @@
 part of '../main.dart';
 
-class FullScreenPage extends StatelessWidget {
+class FullScreenPage extends StatefulWidget {
 
   final Widget child;
 
   const FullScreenPage({Key key, this.child}) : super(key: key);
 
   @override
+  _FullScreenPageState createState() => _FullScreenPageState();
+}
+
+class _FullScreenPageState extends State<FullScreenPage> {
+
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
       child: Center(
-        child: this.child,
+        child: this.widget.child,
       ),
     );
   }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values);
+    super.dispose();
+  }
+
 }
